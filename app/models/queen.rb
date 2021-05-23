0d75dd6d5ff796656d7cd9e921fbf1b325fab8f9
Emmett Pair <elpair@gmail.com>
2021-05-23 00:50:23 -0400

    adds correct bee breeds to queen breed enum

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,5 +1,5 @@
 class Queen < ApplicationRecord
   has_many :hive_queens
   has_many :hives, through: :hive_queens
-  enum breed: [ :one, :two, :three ]
+  enum breed: [ :italian, :russian, :carniolan, :saskatraz, :home_grown ]
 end
