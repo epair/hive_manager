2a60fb7d7a8eb7ecd0ea84618d622dfe7b98c037
Emmett Pair <elpair@gmail.com>
2021-05-31 14:45:30 -0400

    replaces foundation app bar with MenuBar react component

--- app/javascript/packs/app-bundle.js
+++ app/javascript/packs/app-bundle.js
@@ -3,7 +3,10 @@ import ReactOnRails from 'react-on-rails';
 import HivesTable from '../bundles/HivesTable/components/HivesTable';
 import InspectionsTable from '../bundles/InspectionsTable/components/InspectionsTable';
 
+import MenuBar from '../bundles/MenuBar/components/MenuBar';
+
 ReactOnRails.register({
   InspectionsTable,
-  HivesTable
+  HivesTable,
+  MenuBar
 });
