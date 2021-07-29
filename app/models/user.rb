1700ea72d0333f2a4692ded1c6a769483bbd1a1f
Emmett Pair <elpair@gmail.com>
2021-07-29 16:54:07 -0400

    generates JsonWebToken for user before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :hives
+
+  before_save :generate_token
+
+  def generate_token
+    self.token = JsonWebToken.encode({ email: email, password: password })
+  end
 end
