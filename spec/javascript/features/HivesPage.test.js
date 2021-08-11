3ce4a93f9b7fefbb392842b6df0bd5d12d550d12
Emmett Pair <elpair@gmail.com>
2021-08-10 23:40:28 -0400

    adds integration test for HiveForm

--- spec/javascript/features/HivesPage.test.js
+++ spec/javascript/features/HivesPage.test.js
@@ -12,7 +12,6 @@ test('fetches & lists hives', async () => {
   expect(await screen.findByText(/Home/i)).toBeInTheDocument()
 })
 
-
 test('clicking hive name fetches & lists inspections', async () => {
   // rendering App in order to test routing
   render(<App />, { preloadedState: { currentUser: { isLoggedIn: true }}})
@@ -20,4 +19,15 @@ test('clicking hive name fetches & lists inspections', async () => {
   userEvent.click(await screen.findByRole('link', { name: /Home/i }))
 
   expect(await screen.findByText(/2021-08-07/i)).toBeInTheDocument()
+})
+
+test('filling out form and clicking submit adds a hive to the list', async () => {
+  render(<App />, { preloadedState: { currentUser: { isLoggedIn: true }}})
+
+  userEvent.click(await screen.findByText(/Add Hive/i))
+
+  userEvent.type(screen.getByLabelText('Name'), 'Neighbor 1')
+  userEvent.click(screen.getByRole('button', { name: /Create Hive/i }))
+
+  expect(await screen.findByText(/Neighbor 1/i)).toBeInTheDocument()
 })
--- spec/javascript/support/server-handlers.js
+++ spec/javascript/support/server-handlers.js
@@ -4,6 +4,9 @@ const handlers = [
   rest.get('/api/hives', (_, res, ctx) => {
     return res(ctx.json({ hives: [{id: 1, name: 'Home'}]}), ctx.delay(50))
   }),
+  rest.post('/api/hives', (req, res, ctx) => {
+    return res(ctx.json({ hive: {id: 2, name: req.body.hive.name}}), ctx.delay(50))
+  }),
   rest.post('/api/login', (req, res, ctx) => {
     return res(ctx.json({ user: {id: 1, email: req.body.user.email, token: 'secretToken'}}), ctx.delay(50))
   }),
